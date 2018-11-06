0206bb4a973dee14ae235daab433920b77cdcae9
terecrem <43389049+terecrem@users.noreply.github.com>
2018-11-06 14:43:21 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,11 +1,45 @@
-function preload(){
-  // put preload code here
+var video;
+var bottone;
+
+function
+ preload(){
+
 }
 
 function setup() {
-  // put setup code here
+
+createCanvas(500,250);
+background('white');
+video= createCapture(VIDEO);
+video.size(500,500);
+button=createButton('take a selfie');
+button.mousePressed(faifoto);
+}
+
+function faifoto() {
+image(video,0,0);
+
 }
 
 function draw() {
-  // put drawing code here
+//pennello
+  strokeWeight(3);
+  if(mouseIsPressed==true){
+  stroke('#ffd800')
+    // put drawing code here
+  } else{
+noStroke()
+    // put drawing code here
+  }
+
+  line(mouseX,mouseY,pmouseX,pmouseY);
+
+//testo
+  fill('#ffd800');
+  noStroke()
+  textSize(18);
+  textFont('Andale Mono')
+  text("take a selfie and click to draw on yourself",10,20);
+
+
 }
